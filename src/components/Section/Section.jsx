18d28a0070d79aa80c52981b18913a5f466c5d22
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-01-10 22:14:41 +0100

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -3,6 +3,11 @@ import PropTypes from "prop-types";
 import styles from "./Section.module.css";
 
 class Section extends Component {
+    static propTypes = {
+        title: PropTypes.string,
+        children: PropTypes.string,
+    };
+
     render() {
         const { title, children } = this.props;
         return (
@@ -13,10 +18,5 @@ class Section extends Component {
         );
     }
 }
-
-Section.propTypes = {
-    title: PropTypes.string,
-    children: PropTypes.string,
-};
 
 export default Section;
