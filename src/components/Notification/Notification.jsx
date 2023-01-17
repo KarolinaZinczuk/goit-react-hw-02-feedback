f818a8e50ab53c56b7f8f51b8f814dcab4b36697
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-01-17 21:09:05 +0100

    Update Notification.jsx

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,17 +2,17 @@ import { Component } from "react";
 import PropTypes from "prop-types";
 import styles from "./Notification.module.css";
 
-class Notification extends Component {
-    static propTypes = {
-        message: PropTypes.string,    
-    }
-    
+class Notification extends Component {  
     render() {
         const { message } = this.props;
         return (
             <p className={styles.paragraph}>{message}</p>
         ); 
     }
+}
+
+Notification.propTypes = {
+    message: PropTypes.string,    
 }
 
 export default Notification;
